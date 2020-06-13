2d5348f105c8f68ede55af2c49c237747cba2c22
Ege Meriç Erdoğan <egemeric99@gmail.com>
2020-06-13 21:23:47 +0300

    update

---
+++ Server.h
@@ -0,0 +1,75 @@
+#ifndef Server_h
+#define Server_h
+#include <cassert>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+class Server {
+protected:
+    int sockfd, newsockfd, portno;
+    int temp=1;
+    socklen_t clilen;
+    char buffer[1024];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+//public:
+    Server(int Portnum=8888)
+    {
+       this->sockfd=socket(AF_INET,SOCK_STREAM,0);
+       setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &temp, sizeof(temp)); //forece reuse the port
+       if (sockfd<0){
+            assert("Error open socket");
+       }
+        this->serv_addr.sin_family=AF_INET;
+        this->serv_addr.sin_addr.s_addr = INADDR_ANY;
+        this->serv_addr.sin_port=htons(Portnum);
+        if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+              assert("binding statede  problem var");
+        }
+        else{
+            Listen_com();
+            ACCEPT_CON();
+        }
+
+    }
+
+
+    void Listen_com(int queue_tcp=5){
+        listen(this->sockfd,queue_tcp);
+        this->clilen=sizeof(cli_addr);
+    }
+
+    void ACCEPT_CON(){
+        this->newsockfd=accept(this->sockfd,(struct sockaddr *)&cli_addr, &clilen);
+        if(this->newsockfd<0){
+            assert("Error on accept state");
+        }else{
+            printf("Connection from %s\n",inet_ntoa(cli_addr.sin_addr));
+            this->n=read(newsockfd,this->buffer,1024);
+            //printf("\tBuffer:%s:\t",buffer);
+         if (this->n < 0)
+            {assert("ERROR reading from socket");}
+        }
+    }
+
+    void send_msg(const char *msg){
+
+        send(this->newsockfd,msg,strlen(msg),0);
+
+
+
+
+    }
+    public:
+
+
+};
+
+
+
+
+
+
+#endif
--- socket.cpp
+++ socket.cpp
@@ -2,79 +2,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <fstream>
 #include <vector>
-//#include <sstream>
-//#include <netinet/in.h>
-//#include <stdlib.h>
-//#include <sys/types.h>
-//#include <stdlib.h>
+#include "Server.h"
+#include<bits/stdc++.h>
+#define _GLIBCXX_USE_CXX11_ABI 0/1
 void error(const char *msg){
  error(msg);
 
 }
-class Server {
-protected:
-    int sockfd, newsockfd, portno;
-    int temp=1;
-    socklen_t clilen;
-    char buffer[1024];
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
-//public:
-    Server(int Portnum=8888)
-    {
-       this->sockfd=socket(AF_INET,SOCK_STREAM,0);
-       setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &temp, sizeof(temp)); //forece reuse the port
-       if (sockfd<0){
-            error("Error open socket");
-       }
-        this->serv_addr.sin_family=AF_INET;
-        this->serv_addr.sin_addr.s_addr = INADDR_ANY;
-        this->serv_addr.sin_port=htons(Portnum);
-        if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
-              error("binding statede  problem var");
-        }
-        else{
-            Listen_com();
-            ACCEPT_CON();
-        }
-
-    }
-
-
-    void Listen_com(int queue_tcp=5){
-        listen(this->sockfd,queue_tcp);
-        this->clilen=sizeof(cli_addr);
-    }
-
-    void ACCEPT_CON(){
-        this->newsockfd=accept(this->sockfd,(struct sockaddr *)&cli_addr, &clilen);
-        if(this->newsockfd<0){
-            error("Error on accept state");
-        }else{
-            printf("Connection from %s\n",inet_ntoa(cli_addr.sin_addr));
-            this->n=read(newsockfd,this->buffer,1024);
-            //printf("\tBuffer:%s:\t",buffer);
-         if (this->n < 0)
-            {error("ERROR reading from socket");}
-        }
-    }
-
-    void send_msg(const char *msg){
-
-        send(this->newsockfd,msg,strlen(msg),0);
-
-
-
-
-    }
-    public:
-
-
-};
 
 
 class HttpServer :protected Server{
@@ -104,8 +40,11 @@ protected:
       if(!html_page.is_open()){
         html_page.close();
         //perror("\n\nCant open the index file\n");
-        this->data="<html><body>Not found</body></html>";
-        this->content_length=data.length();
+
+        this->data=data.append("<html><body>Not found</body></html>"); //todo :not found page
+        this->content_length=data.length()-2;
+
+
       }else{
           while(!html_page.eof()){
                 getline(html_page,line);
@@ -117,9 +56,28 @@ protected:
       //std::cout<<data;
     }
 
+  /*  void set_headers(){
+    bool dot_is_found=false;
+    std::string MIME_TYPE=std::string(requested_page);
+    int req_size=MIME_TYPE.length()-1;
+    std::string temp;
+    for(int ct=req_size;(MIME_TYPE[ct]!='.'&&ct>0);ct--){
+       temp.push_back(MIME_TYPE[ct]);
+
+    }
+    reverse(temp.begin(),temp.end());
+    if(temp)
+
+*/
+
+
+
+
+
 public:
     void send_HTTP(){
         this->decode_client_header();
+       // this->set_headers();
         //std::cout<<"ok";
         this->read_file(requested_page);
         std::string msg_string= this->ok + this->content + this->content_ct + std::to_string(this->content_length) + this->data; //concatenate all http headers
@@ -127,7 +85,7 @@ public:
          char send_msg_char[msg_len+1];
          strcpy(send_msg_char, msg_string.c_str());
 
-        //printf("%s",send_msg_char);
+        printf("---%s----",send_msg_char);
         this->send_msg(send_msg_char);
         close(newsockfd);
         close(sockfd);
@@ -159,7 +117,7 @@ public:
 };
 
 int main(void){
-
+            std::cout<<"Your site works on port 8888\n";
             while(1){
                 HttpServer *HTTP= new HttpServer;
 
