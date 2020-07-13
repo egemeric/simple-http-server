484a4cac122430fc30b3e337d75e9802aadb902d
Bahadır <bahadirkundak@pm.me>
2020-07-13 21:19:05 +0300

    Update socket.cpp

--- socket.cpp
+++ socket.cpp
@@ -4,21 +4,21 @@
 #include <string.h>
 #include <fstream>
 #include <vector>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include "Server.h"
 #include "httpserver.h"
 #include "error.h"
-#define _GLIBCXX_USE_CXX11_ABI 0/1
+#define _GLIBCXX_USE_CXX11_ABI 0 / 1
 
+int main(void)
+{
+        std::cout << "Your site works on port 8888\n";
 
-
-int main(void){
-            std::cout<<"Your site works on port 8888\n";
-
-            while(1){
-                    HttpServer *HTTP= new HttpServer;
-                    HTTP->send_HTTP();
-                    usleep(300);
-                    delete HTTP;
-            }
+        while (1)
+        {
+                HttpServer *HTTP = new HttpServer;
+                HTTP->send_HTTP();
+                usleep(300);
+                delete HTTP;
+        }
 }
